64bd398f0a3c1a17dfd36f0c8f018a4d4c1e06ae
Ammar Elbehery <ammarelbehery04@gmail.com>
2026-06-18 04:25:33 +0300

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,18 @@
+// @ts-check
+import { defineConfig, fontProviders } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  fonts: [
+    {
+      provider: fontProviders.google(),
+      name: "EB Garamond",
+      cssVariable: "--font-garamond",
+      weights: ["400 700"],
+    },
+  ],
+});
